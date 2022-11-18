a3877afaf362ad720e95ef7cf61e37f2d1e37f09
AlexanderAndrusenko <aaausaaa@gmail.com>
2022-11-18 09:37:49 +0200

    Initial commit

---
+++ TeachYourAI/TeachYourAI/TeachYourAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeachYourAIApp.swift
+//  TeachYourAI
+//
+//  Created by Alexander Andrusenko on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TeachYourAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
